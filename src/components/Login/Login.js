d4787d2e86e8a535261b44298398d8a7bdf291a3
TFVO11 <cosmoyj@naver.com>
2022-07-20 15:51:04 +0900

    section10_2_8

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from 'react';
+import React, { useState, useEffect, useReducer, useContext, useRef } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -37,10 +37,13 @@ const Login = (props) => {
   const [emailState, dispatchEmail] = useReducer(emailReducer, { value: '', isValid: null });
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, { value: '', isValid: null });
 
-  const { isValid: emailIsValid } = emailState;
-  const { isValid: passwordIsValid } = passwordState;
+  // const { isValid: emailIsValid } = emailState;
+  // const { isValid: passwordIsValid } = passwordState;
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
 
   //구조분해 할당으로 useEffect가 추가적으로 돌아가는 것을 막아준다.
@@ -94,36 +97,44 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+        emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <Input 
-        id="email" 
-        label="E-Mail" type="email" 
-        isValid={emailIsValid} 
-        value={emailState.value} 
-        onChange={emailChangeHandler} 
-        onBlur={validateEmailHandler}
+        <Input
+          ref={emailInputRef}
+          id="email"
+          label="E-Mail" type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
         />
         <div
           className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ''
             }`}
         >
           <label htmlFor="password">Password</label>
-          <Input 
-        id="password" 
-        label="Password" type="password" 
-        isValid={passwordIsValid} 
-        value={passwordState.value} 
-        onChange={passwordChangeHandler} 
-        onBlur={validatePasswordHandler}
-        />
+          <Input
+            ref={passwordInputRef}
+            id="password"
+            label="Password" type="password"
+            isValid={passwordIsValid}
+            value={passwordState.value}
+            onChange={passwordChangeHandler}
+            onBlur={validatePasswordHandler}
+          />
         </div>
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} >
             Login
           </Button>
         </div>
